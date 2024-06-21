8f9ccefe3667cd24230d956e82485f423b8de422
Manohar jha <aryabdwaj26@gmail.com>
2024-06-21 14:51:17 -0500

    update login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,11 @@ const Login = () => {
             <div className='absolute'>
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="background" />
             </div>
+<<<<<<< HEAD
             <form onChange={e => e.preventDefault()} className='rounded-lg absolute w-3/12 bg-black bg-tr p-12 my-36 mx-auto right-0 left-0 text-white bg-opacity-8s0'>
+=======
+            <form className='rounded-lg absolute w-3/12 bg-black bg-tr p-12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
+>>>>>>> 5ed25a7 (update login page)
                 <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign in" : "Sign up"}</h1>
                 {!isSignInForm && <input type="name" placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700 ' />}
                 <input type="text" placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700 ' />
